9527822429b04f1a2943f296dbdcdffa94ce7fdf
Albert Li <41499883+Albli001348@users.noreply.github.com>
2019-09-18 13:38:08 -0700

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key ="DdBcMdIRye7yzEXVWfSq2q7nO"
-consumer_secret ="cWRaltydOsOR48bdyORuyrMI1w3caYZKKn0MbOLZwsx7KqUX3U"
-access_token ="1173753649245245440-TkbWuHxCLR32FGeS2YLnuuP3Ym2CYK"
-access_token_secret ="VnDNHANQHIL1O2UHl9Nbka59jlp5ODb9XZXXIpiwknR28"
+consumer_key =
+consumer_secret =
+access_token =
+access_token_secret =
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 auth.set_access_token(access_token, access_token_secret) 
